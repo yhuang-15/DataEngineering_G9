77a5eea99590d85ac3f4053eebc6ee63e5bfac9b
Steven <y.huang_15@tilburguniversity.edu>
2021-10-05 12:56:39 +0200

    ingestion & db

--- Assignment_1/data-ingestion-cp/app.py
+++ Assignment_1/data-ingestion-cp/app.py
@@ -1,25 +1,24 @@
 import os
 
-import pandas as pd
 import requests
 from flask import Flask, Response
 
-from flask import jsonify
-from resources import model_trainer
+from resources import download_sklearn_data
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/training-cp/model', methods=['POST'])
-def train_models():
+@app.route('/data-ingestion', methods=['PUT'])
+def save_data():
     db_api = os.environ['TRAININGDB_API']
-    # Make a GET request to training db service to retrieve the training data/features.
-    r = requests.get(db_api)
-    j = r.json()
-    df = pd.DataFrame.from_dict(j)
-    resp = model_trainer.train(df.values)
-    return resp
+
+    content = download_sklearn_data.load()
+
+    # Make a PUT request to training db service to store data into the training data/features.
+    r = requests.put(db_api, data=content)
+
+    return r.content
 
 
 app.run(host='0.0.0.0', port=5000)
---
+++ Assignment_1/data-ingestion-cp/resources/download_sklearn_data.py
@@ -0,0 +1,23 @@
+from sklearn import datasets
+import numpy as np
+
+def load():
+    # load data from sklearn
+    X, y = datasets.load_digits(return_X_y=True)
+    row_n = X.shape[0]
+    column_n = y.shape[1] + 1
+
+    all_data = np.random.rand(row_n, column_n)
+
+    all_data[:, :-1] = X
+    all_data[:, -1] = y
+
+    # convert the data into correct request.put json form
+    data_list = []
+    for row in all_data:
+        dict_1 = {f"pix_{i}": row[i] for i in range(row.shape[0] - 1)}
+        dict_1['y'] = row[-1]
+
+        data_list.append(dict_1)
+
+    return data_list
