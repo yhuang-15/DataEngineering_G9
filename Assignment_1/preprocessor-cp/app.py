a5c0efa7d376776ed861fbe92bc94dd0708844e1
Steven <y.huang_15@tilburguniversity.edu>
2021-10-09 17:08:12 +0200

    preprocessor_compose_file

--- Assignment_1/preprocessor-cp/app.py
+++ Assignment_1/preprocessor-cp/app.py
@@ -13,8 +13,9 @@ app.config["DEBUG"] = True
 
 
 @app.route('/preprocessing-cp/pca', methods=['POST'])
-def train_models():
+def preprocess():
     db_api = os.environ['TRAININGDB_API']
+    db_save_api = os.environ['SAVE_API']
     # Make a GET request to training db service to retrieve the training data/features.
     r = requests.get(db_api)
     j = r.json()
@@ -23,7 +24,7 @@ def train_models():
     resp_np = resp
 
     headers = {"Content-Type": "application/json"}
-    response = requests.request("PUT", db_api, json=resp_np, headers=headers)
+    response = requests.request("PUT", db_save_api, json=resp_np, headers=headers)
 
     return response.content
 
