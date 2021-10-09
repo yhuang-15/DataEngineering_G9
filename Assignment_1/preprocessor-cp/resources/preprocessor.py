32d6af1104850b48826a7247bb9fdb4b1cd591f5
Bocsárdi Gergő <bocsardi.gergely@gmail.com>
2021-10-09 16:32:57 +0200

    fixed prerpoc

--- Assignment_1/preprocessor-cp/resources/preprocessor.py
+++ Assignment_1/preprocessor-cp/resources/preprocessor.py
@@ -6,7 +6,6 @@ from scipy.sparse import data
 from sklearn.decomposition import PCA
 import pandas as pd
 
-
 def clean(dataset):
     # need to get the data to X and y separately
     y = dataset[:,-1]
@@ -16,7 +15,14 @@ def clean(dataset):
 
     pca.fit(X)
     X_clean = pca.transform(X)
+    y_2d = np.reshape(y, (-1, 1))
 
-    clean_data = np.append(X_clean, y, axis=1)
+    clean_data = np.hstack((X_clean, y_2d))
 
-    return clean_data
+    data_list = []
+    for row in clean_data:
+        dict_1 = {f"pix_{i}": row[i] for i in range(row.shape[0] - 1)}
+        dict_1["y"] = row[-1]
+
+        data_list.append(dict_1)
+    return data_list
