715c3c4f60d94db84e833bf6cfad7116b0f51896
Bocsárdi Gergő <bocsardi.gergely@gmail.com>
2021-10-09 17:00:49 +0200

    second trainer done

--- Assignment_1/training-cp-gcs-2/resources/model_trainer.py
+++ Assignment_1/training-cp-gcs-2/resources/model_trainer.py
@@ -5,46 +5,45 @@ import os
 
 from flask import jsonify
 from google.cloud import storage
-from keras.layers import Dense
-from keras.models import Sequential
 
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 def train(dataset):
     # split into input (X) and output (Y) variables
-    X = dataset[:, 0:8]
-    Y = dataset[:, 8]
+    X = dataset[:, 0:-1]
+    y = dataset[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
     # define model
-    model = Sequential()
-    model.add(Dense(12, input_dim=8, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    # compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # Fit the model
-    model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
-    # evaluate the model
-    scores = model.evaluate(X, Y, verbose=0)
-    print(model.metrics_names)
+    clf = RandomForestClassifier()
+    clf.fit(X_train, y_train)
+
+    scores = clf.score(X_test, y_test)
+    print(scores)
+
     text_out = {
-        "accuracy:": scores[1],
-        "loss": scores[0],
+        "accuracy:": scores
     }
-    # Saving model in a given location provided as an env. variable
+
     project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
     model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
     if model_repo:
         # Save the model localy
-        model.save('local_model.h5')
-        # Save to GCS as model.h5
+        with open("local_random_forest_model.pkl", "wb") as file:
+            pickle.dump(model, file)
+        # Save to GCS as random_forestn_model.pkl
         client = storage.Client(project=project_id)
         bucket = client.get_bucket(model_repo)
-        blob = bucket.blob('model.h5')
+        blob = bucket.blob('random_forest_model.pkl')
         # Upload the locally saved model
-        blob.upload_from_filename('local_model.h5')
+        blob.upload_from_filename('local_random_forest_model.pkl')
         # Clean up
-        os.remove('local_model.h5')
+        os.remove('local_random_forest_model.pkl')
         logging.info("Saved the model to GCP bucket : " + model_repo)
         return jsonify(text_out), 200
     else:
-        model.save("model.h5")
+        with open("local_random_forest_model.pkl", "wb") as file:
+            pickle.dump(model, file)
         return jsonify({'message': 'The model was saved locally.'}), 200
